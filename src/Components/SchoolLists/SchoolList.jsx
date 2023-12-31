ef049acd6a77295226b335a6f4155ff36259b084
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-12-31 17:29:48 +0500

    img

--- src/Components/SchoolLists/SchoolList.jsx
+++ src/Components/SchoolLists/SchoolList.jsx
@@ -1,6 +1,11 @@
 import School from './School'
 import Footer from '../Footer'
 
+import FFMSImage from '../SchoolLists/Images/FFMS.jpg';
+import PakturkImage from '../SchoolLists/Images/Pakturk/Pakturk.jpg';
+
+
+
 export default function SchoolList() {
   return (
     <div >
@@ -20,12 +25,12 @@ export default function SchoolList() {
             flexDirection: 'column',
           }
       }}>
-      <School name="Pakturk International Marif" img="..\src\Components\SchoolLists\Images\FFMS.jpg" color="red" rating='4.5/5'/>
-      <School name="Fauji Foundation Model School" img="..\src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="purple" rating='4/5'/>
-      <School name="Beacon House Schools" img="..\src\Components\SchoolLists\Images\FFMS.jpg" color="green" rating='3.3/5'/>
-      <School name="Islamia Model High School" img="..\src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="red" rating='4/5'/>
-      <School name="St Francis Schooling System" img="..\src\Components\SchoolLists\Images\FFMS.jpg" color="purple" rating='3/5'/>
-      <School name="Pakturk International Marif" img="..\src\Components\SchoolLists\Images\Pakturk\Pakturk.jpg" color="green" rating='3.5/5'/>
+      <School name="Pakturk International Marif" img={PakturkImage} color="red" rating='4.5/5'/>
+      <School name="Fauji Foundation Model School" img={FFMSImage} color="purple" rating='4/5'/>
+      <School name="Beacon House Schools" img={PakturkImage} color="green" rating='3.3/5'/>
+      <School name="Islamia Model High School" img={FFMSImage} color="red" rating='4/5'/>
+      <School name="St Francis Schooling System" img={PakturkImage}color="purple" rating='3/5'/>
+      <School name="Pakturk International Marif" img={FFMSImage} color="green" rating='3.5/5'/>
 
       
         </div>
